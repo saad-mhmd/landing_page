21bfd1259d16bd3ff0950c588dd9536f85df44b3
saad-junia <mohammad.saad@student.junia.com>
2024-06-17 09:12:54 +0200

    Reset option

--- js/master.js
+++ js/master.js
@@ -300,3 +300,16 @@ bulletsSpan.forEach((span) => {
     handleActive(e);
   });
 });
+
+// Reset Button
+document.querySelector(".reset-options").onclick = function () {
+    
+    // localStorage.clear();
+    localStorage.removeItem("color_option");
+    localStorage.removeItem("background_option");
+    localStorage.removeItem("bullets_option");
+
+  // Reload Window
+  window.location.reload();
+  
+};
